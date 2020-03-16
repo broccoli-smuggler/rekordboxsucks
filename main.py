bcb6099a4950d0c6d7cd4475faca8ab7c86fdc43
broccoli <joshuansjordan@gmail.com>
2020-03-16 19:12:09 +0100

    added help

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
+import sys
+import getopt
 from xml_helper import XmlHelper
-import flac
 
 
-def run():
-    xml = XmlHelper("rekordbox.xml")
-    print(
-        "\tWelcome to rekordbox sucks.\n",
-        "Please select an option:",
-        "1. "
-    )
+def run(argv):
+    if len(argv) != 1:
+        print('Please provide the rekordbox.xml file')
+        return
+    try:
+        opts, args = getopt.getopt(argv, "h")
+    except getopt.GetoptError:
+        print('main.py <rekordboxfile>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print('main.py <rekordboxfile>')
+            sys.exit()
+
+    xml = XmlHelper(str(sys.argv[1]))
 
     # xml.change_library_location("/Users/login/Desktop/", "/Users/broccoli/Music/")
-    # xml.fix_missing_files_with_wrong_ext()
+    xml.fix_missing_files_with_wrong_ext()
     xml.export_new_library()
 
 
 if __name__ == "__main__":
-    run()
+    run(sys.argv[1:])
